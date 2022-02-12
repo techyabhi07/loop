b47fb39b6b33df2e138502862a206dc85035b10f
techyabhi07 <rajabhinav1702@gmail.com>
2022-02-12 23:55:55 +0530

    added some new forloop programs

---
+++ whileloop8.c
@@ -0,0 +1,17 @@
+/*write a program to find the factorial value of a number*/
+
+#include<stdio.h>
+
+int main(){
+    int num,i,fact;
+    printf("enter the number;");
+    scanf("%d",&num);
+    fact==i= 1;
+    while (i<=num)
+    {
+        fact=fact*i;
+        i+=
+    }
+    
+return 0;
+}
